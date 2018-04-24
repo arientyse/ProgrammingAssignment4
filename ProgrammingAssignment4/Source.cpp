d79443347e0093e8091e0419df37d0fb642b0662
Ariana Tyson <aripiggy@hotmail.com>
2018-04-24 19:44:06 -0400

    Code for reading from a text file.

--- ProgrammingAssignment4/Source.cpp
+++ ProgrammingAssignment4/Source.cpp
@@ -4,7 +4,53 @@
 
 using namespace std;
 
+const int LISTSIZE = 4;
+const int STRINGSIZE = 30;
+
+typedef char STRING30[STRINGSIZE];
+typedef STRING30 LISTNAMES[LISTSIZE];
+
 int main()
 {
+	LISTNAMES firstname;
+	char lastname[LISTSIZE];
+	int daysofrental[LISTSIZE];
+	double balancedue[LISTSIZE];
 
+	int i = 0;
+
+	ifstream in;
+	in.open("C:\\users\\aripi\\Documents\\ProjectTextFiles\\invoice1_test1.txt");
+
+	if (in.is_open())
+	{
+		while (!in.eof())
+		{
+			in >> lastname[i] >> firstname[i] >> daysofrental[i] >> balancedue[i];
+			i++;
+		}
+		in.close();
+	}
+	else
+		cout << "File failed to open" << endl;
+
+	for (int idx = 0; idx < LISTSIZE; idx++)
+	{
+		cout << setw(10);
+		cout << firstname[idx];
+
+		cout << setw(10);
+		cout << lastname[idx];
+
+		cout << setw(10);
+		cout << daysofrental[idx];
+
+		cout << setw(10);
+		cout << balancedue[idx];
+
+		cout << endl;
+	}
+
+	system("PAUSE");
+	return 0;
 }
